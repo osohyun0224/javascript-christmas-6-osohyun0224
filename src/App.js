da5281f1bb997b6c9935e39c1e1b77f90b790623
osohyun0224 <osohyun0224@naver.com>
2023-11-13 21:12:43 +0900

    ✨ feat(OutputView): 예상 방문 날짜가 반영된 안내 시작 메세지 출력 기능 구현
    - 입력 받은 예상 방문 날짜가 반영된 안내 시작 메세지를 출력하는 기능을 OutputView.js에 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -10,53 +10,48 @@ import CalculateEventBadge from "./domain/CalculateEventBadge";
 import { WOOTECO_MENU_PRICES } from "./constants/Constant";
 
 class App {
-  constructor() {
-    this.menuPrices = WOOTECO_MENU_PRICES;
-    this.originTotalPriceCalculator = new CalculateOriginTotalPrice(this.menuPrices);
-    this.dDayDiscountCalculator = new CalculateDDayDiscount();
-    this.dailyDiscountCalculator = new CalculateDailyDiscount(this.menuPrices);
-    this.specialDiscountCalculator = new CalculateSpecialDiscount();
-    this.giveawayEventCalculator = new CalculateGiveawayEvent(this.menuPrices);
-    this.eventBadgeCalculator = new CalculateEventBadge();
-  }
-
-  async run() {
-    OutputView.printIntroMessage();
-    try {
-      const visitDate = await InputView.readDate();
-      const orderItems = await InputView.readOrder();
-      this.calculateResult(visitDate,orderItems);
-    } catch (error) {
-      Console.print(error.message);
-    }
-  }
-
-  calculateResult(visitDate, orderItems) {
-    const totalOrderAmount = this.originTotalPriceCalculator.calculate(orderItems);
-    let totalDiscount = this.dDayDiscountCalculator.calculate(visitDate);
-
-    const { totalDiscount: dailyDiscount, discountsApplied: dailyDiscountsApplied } = this.dailyDiscountCalculator.calculate(visitDate, orderItems);
-    
-    totalDiscount += dailyDiscount;
-    let discountsApplied = [...dailyDiscountsApplied];
-    
-    if (totalDiscount > 0) {
-      discountsApplied.push(`크리스마스 디데이 할인: -${this.formatCurrency(totalDiscount)}`);
+    constructor() {
+        this.menuPrices = WOOTECO_MENU_PRICES;
+        this.originTotalPriceCalculator = new CalculateOriginTotalPrice(this.menuPrices);
+        this.dDayDiscountCalculator = new CalculateDDayDiscount();
+        this.dailyDiscountCalculator = new CalculateDailyDiscount(this.menuPrices);
+        this.specialDiscountCalculator = new CalculateSpecialDiscount();
+        this.giveawayEventCalculator = new CalculateGiveawayEvent(this.menuPrices);
+        this.eventBadgeCalculator = new CalculateEventBadge();
     }
 
-    const { totalDiscount: specialDiscount, discountsApplied: specialDiscountsApplied } = this.specialDiscountCalculator.calculate(visitDate);
-    totalDiscount += specialDiscount;
-    discountsApplied.push(...specialDiscountsApplied);
+    async run() {
+        OutputView.printIntroMessage();
+        try {
+            const visitDate = await InputView.readDate();
+            const orderItems = await InputView.readOrder();
+            this.calculateResult(visitDate, orderItems);
+        } catch (error) {
+            Console.print(error.message);
+        }
+    }
 
-    const { totalDiscount: giveawayDiscount, discountsApplied: giveawayDiscountsApplied, giftedItem } = this.giveawayEventCalculator.calculate(totalOrderAmount);
-    totalDiscount += giveawayDiscount;
-    discountsApplied.push(...giveawayDiscountsApplied);
+    calculateResult(visitDate, orderItems) {
+        const totalOrderAmount = this.originTotalPriceCalculator.calculate(orderItems);
+        let totalDiscount = this.dDayDiscountCalculator.calculate(visitDate);
 
-    let finalAmount = totalOrderAmount - (totalDiscount - giveawayDiscount);
-    
-    let badge = this.eventBadgeCalculator.calculate(totalDiscount);
+        const { totalDiscount: dailyDiscount, discountsApplied: dailyDiscountsApplied } = this.dailyDiscountCalculator.calculate(visitDate, orderItems);
+        totalDiscount += dailyDiscount;
+        let discountsApplied = [...dailyDiscountsApplied];
 
-  }
+        const { totalDiscount: specialDiscount, discountsApplied: specialDiscountsApplied } = this.specialDiscountCalculator.calculate(visitDate);
+        totalDiscount += specialDiscount;
+        discountsApplied.push(...specialDiscountsApplied);
+
+        const { totalDiscount: giveawayDiscount, discountsApplied: giveawayDiscountsApplied, giftedItem } = this.giveawayEventCalculator.calculate(totalOrderAmount);
+        totalDiscount += giveawayDiscount;
+        discountsApplied.push(...giveawayDiscountsApplied);
+
+        let finalAmount = totalOrderAmount - (totalDiscount - giveawayDiscount);
+        let badge = this.eventBadgeCalculator.calculate(totalDiscount);
+
+        OutputView.printEventPreview(visitDate);
+    }
 }
 
 export default App;
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -3,8 +3,12 @@ import { GUIDE_MESSAGE } from "../constants/Message";
 
 const OutputView = {
     printIntroMessage() {
-    Console.print(GUIDE_MESSAGE.intro);
-    }
-}
+        Console.print(GUIDE_MESSAGE.intro);
+    },
+
+    printEventPreview(visitDate) {
+        Console.print(`${GUIDE_MESSAGE.startEventMonth}${visitDate}${GUIDE_MESSAGE.startEventDay}${GUIDE_MESSAGE.startEventGuide}\n`);
+    },
+};
 
 export default OutputView;
