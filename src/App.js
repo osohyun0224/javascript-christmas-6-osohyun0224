0133f7fc25a6720cc868c5e2866c02edf436d25a
osohyun0224 <osohyun0224@naver.com>
2023-11-14 13:00:34 +0900

    ♻️ refactor(ChristmasController): 전체 이벤트 로직을 제어하는 컨트롤러 구현
    - App 클래스에서 진행되었던 전체 이벤트 진행을 제어하는 기능의 컨트롤러를 생성하여 구현하였습니다.
    - 애플리케이션의 메인 흐름을 Controller이 관리하게 되고, App은 이를 초기화하고 실행하는 역할을 담당하도록 리팩토링을 진행하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,63 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
-import InputView from "./views/InputView";
-import OutputView from "./views/OutputView";
-import CalculateOriginTotalPrice from "./domain/CalculateOriginTotalPrice";
-import CalculateDDayDiscount from "./domain/CalculateDDayDiscount";
-import CalculateDailyDiscount from "./domain/CalculateDailyDiscount";
-import CalculateSpecialDiscount from "./domain/CalculateSpecialDiscount";
-import CalculateGiveawayEvent from "./domain/CalculateGiveawayEvent";
-import CalculateEventBadge from "./domain/CalculateEventBadge";
-import { WOOTECO_MENU_PRICES } from "./constants/Constant";
+import ChristmasController from "./controller/ChristmasController";
 
 class App {
-    constructor() {
-        this.menuPrices = WOOTECO_MENU_PRICES;
-        this.originTotalPriceCalculator = new CalculateOriginTotalPrice(this.menuPrices);
-        this.dDayDiscountCalculator = new CalculateDDayDiscount();
-        this.dailyDiscountCalculator = new CalculateDailyDiscount(this.menuPrices);
-        this.specialDiscountCalculator = new CalculateSpecialDiscount();
-        this.giveawayEventCalculator = new CalculateGiveawayEvent(this.menuPrices);
-        this.eventBadgeCalculator = new CalculateEventBadge();
-    }
-
     async run() {
-        OutputView.printIntroMessage();
-        try {
-            const visitDate = await InputView.readDate();
-            const orderItems = await InputView.readOrder();
-            this.calculateResult(visitDate, orderItems);
-        } catch (error) {
-            Console.print(error.message);
-        }
-    }
-
-    calculateResult(visitDate, orderItems) {
-        const totalOrderAmount = this.originTotalPriceCalculator.calculate(orderItems);
-        let totalDiscount = this.dDayDiscountCalculator.calculate(visitDate);
-
-        const { totalDiscount: dailyDiscount, discountsApplied: dailyDiscountsApplied } = this.dailyDiscountCalculator.calculate(visitDate, orderItems);
-        totalDiscount += dailyDiscount;
-        let discountsApplied = [...dailyDiscountsApplied];
-
-        const { totalDiscount: specialDiscount, discountsApplied: specialDiscountsApplied } = this.specialDiscountCalculator.calculate(visitDate);
-        totalDiscount += specialDiscount;
-        discountsApplied.push(...specialDiscountsApplied);
-
-        const { totalDiscount: giveawayDiscount, discountsApplied: giveawayDiscountsApplied, giftedItem } = this.giveawayEventCalculator.calculate(totalOrderAmount);
-        totalDiscount += giveawayDiscount;
-        discountsApplied.push(...giveawayDiscountsApplied);
-
-        let finalAmount = totalOrderAmount - (totalDiscount - giveawayDiscount);
-        let badge = this.eventBadgeCalculator.calculate(totalDiscount);
-
-        OutputView.printEventPreview(visitDate);
-        OutputView.printOrderMenu(orderItems);
-        OutputView.printOriginTotalPrice(totalOrderAmount);
-        OutputView.printGiveawayMenu(giftedItem);
-        OutputView.printBenefitDetails(discountsApplied);
-        OutputView.printTotalBenefitAmount(totalDiscount);
-        OutputView.printDiscountedTotalAmount(finalAmount);
-        OutputView.printEventBadge(badge);
+        const eventController = new ChristmasController();
+        await eventController.runWootecoEvent();
     }
 }
 
