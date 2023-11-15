2409e6b76629b9ba3b5c653ae36b14d04d83b179
osohyun0224 <osohyun0224@naver.com>
2023-11-15 11:36:44 +0900

    🐛 fix(CalculateDDayDiscountTest): 수정된 크리스마스 할인 로직에 따라 테스트 코드 수정
    - 기존의 크리스마스 할인 로직이 구현된 대로 작성된 테스트 코드의 버그를 올바르게 수정하였습니다.
    - CalculateDDayDiscount 클래스의 calculate 메소드가 객체를 반환하도록 변경되어서 이에 맞게 테스트 코드를 수정했습니다.
    -  totalDiscount 필드만을 검증하는 것으로 테스트 케이스를 수정했습니다.

--- __tests__/CalculateDDayDiscountTest.js
+++ __tests__/CalculateDDayDiscountTest.js
@@ -9,18 +9,21 @@ describe("크리스마스 디데이 할인 계산 로직 테스트", () => {
 
   test("D-Day 할인이 적용되지 않는 날짜에는 할인 금액이 0원이어야 한다", () => {
     const nonDiscountDay = 25 + 1; 
-    expect(dDayDiscountCalculator.calculate(nonDiscountDay)).toBe(0);
+    const result = dDayDiscountCalculator.calculate(nonDiscountDay);
+    expect(result.totalDiscount).toBe(0);
   });
 
   test("D-Day 할인이 적용되는 날짜에는 올바른 할인 금액을 반환해야 한다", () => {
     const discountDay = 1; 
     const expectedDiscount = 1000;
-    expect(dDayDiscountCalculator.calculate(discountDay)).toBe(expectedDiscount);
+    const result = dDayDiscountCalculator.calculate(discountDay);
+    expect(result.totalDiscount).toBe(expectedDiscount);
   });
 
   test("D-Day 할인이 적용되는 범위 내의 날짜에 대해서는 증가하는 할인 금액을 반환해야 한다", () => {
     const discountDay = 1 + 5; 
     const expectedDiscount = 1000 + (5 * 100);
-    expect(dDayDiscountCalculator.calculate(discountDay)).toBe(expectedDiscount);
+    const result = dDayDiscountCalculator.calculate(discountDay);
+    expect(result.totalDiscount).toBe(expectedDiscount);
   });
 });
